2cc4b5821e92dcfca956e774ab04c21678e0f967
Jacob Immich <jacobimmich@gmail.com>
2023-11-27 16:15:12 -0500

    Extract tar archive

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,9 @@ git archive --format tar --output ./project.tar demo
 
 echo "Uploading project..."
 rsync --progress --stats ./project.tar jacobimmich@storm.cise.ufl.edu:/cise/homes/jacobimmich/project.tar
+
+ssh jacobimmich@storm.cise.ufl.edu << ENDSSH
+    mkdir -p project
+    rm -rf ./project/*
+    tar -xvf ./project.tar -C ./project
+ENDSSH
