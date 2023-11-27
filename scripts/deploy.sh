f466b0e76eb65857e02c6c5a2b7d460544963ab5
Jacob Immich <jacobimmich@gmail.com>
2023-11-27 16:04:42 -0500

    Update deploy script for storm.cise.ufl.edu

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ echo "Generating TAR archive"
 git archive --format tar --output ./project.tar demo
 
 echo "Uploading project..."
-rsync --progress --stats ./project.tar jacobimmich@lin114-00.cise.ufl.edu:/cise/homes/jacobimmich/project.tar
+rsync --progress --stats ./project.tar jacobimmich@storm.cise.ufl.edu:/cise/homes/jacobimmich/project.tar
