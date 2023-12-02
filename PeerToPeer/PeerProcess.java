d1bf0cb5d8c6dd81d237fae1558471a372e26723
samlonneman <56888420+SamLonneman@users.noreply.github.com>
2023-12-02 16:49:30 -0500

    Optimizations

--- PeerToPeer/PeerProcess.java
+++ PeerToPeer/PeerProcess.java
@@ -24,7 +24,7 @@ public class PeerProcess
     // Global debug constants
     public static final boolean WRITE_LOGS = true;
     public static final boolean PRINT_LOGS = true;
-    public static final boolean PRINT_ERRS = true;
+    public static final boolean PRINT_ERRS = false;
 
     // Global message type constants
     public static final byte CHOKE = 0;
@@ -296,6 +296,9 @@ public class PeerProcess
             peerDownloadRates.put(peerSocket, 0);
             pendingPieces.put(peerSocket, -1);
             peerSockets.add(peerSocket);
+            // When a new peer is added and we have room for more preferred neighbors, update preferred neighbors
+            if (peerSockets.size() < numberOfPreferredNeighbors)
+                timeToUpdatePreferredNeighbors = true;
             sendMessage(peerOut, new HandshakeMessage(id));
         } catch (IOException e) {
             error("Error initializing peer.");
@@ -356,10 +359,10 @@ public class PeerProcess
                         break;
                     }
                 }
-                // If all peers have finished, wait for 2 seconds to let messages finish propagating, then exit
+                // If all peers have finished, wait for a second to let messages finish propagating, then exit
                 if (allPeersFinished) {
                     try {
-                        Thread.sleep(2000);
+                        Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
