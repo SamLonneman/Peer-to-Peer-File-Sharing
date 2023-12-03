8c700b610e32625e3e3332e3a75e6673c3b27030
Jacob Immich <90436609+joeymich@users.noreply.github.com>
2023-12-03 17:16:58 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 echo "Generating TAR archive"
-git archive --format tar --output ./project.tar demo
+git archive --format tar --output ./project.tar main
 
 echo "Uploading project..."
-rsync --progress --stats ./project.tar jacobimmich@storm.cise.ufl.edu:/cise/homes/jacobimmich/project.tar
+rsync --progress --stats ./project.tar $USER@storm.cise.ufl.edu:/cise/homes/$USER/project.tar
 
-ssh jacobimmich@storm.cise.ufl.edu << ENDSSH
+ssh $USER@storm.cise.ufl.edu << ENDSSH
     mkdir -p project
     rm -rf ./project/*
     tar -xvf ./project.tar -C ./project
